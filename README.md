db6be7ddd8c100f3935814fd37605ab80169594a
Peskov Sergey <devpeskov@proton.me>
2023-01-09 17:18:17 +0600

    Docker: add auto-initialization of admin-user

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ FLASK_SECRET_KEY="flask-secure-r&+q;(a!#37goqw,er2/.[aktt2&5e#%hegx-13o((=uz^o"
 SECURITY_PASSWORD_SALT="saltjkl12jht89;sdf"
 SECURITY_PASSWORD_HASH="bcrypt"
 
+FLASK_ADMIN_EMAIL="admin@test.com"
+FLASK_ADMIN_PASSWORD="abc12345"
+
 SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}"' > .env
 ```
 - `docker-compose up --build`
--- backend/boot.sh
+++ backend/boot.sh
@@ -12,4 +12,22 @@ echo "PostgreSQL started"
 
 flask db upgrade
 
+python -c "from app import app, db, user_datastore
+from models import User, Role
+app.app_context().push()
+
+is_object_exists = db.session.query(
+    db.exists().where(User.email == '$FLASK_ADMIN_EMAIL')
+).scalar()
+print(f'Is admin exists? -{is_object_exists}')
+if not is_object_exists:
+    user_datastore.create_user(email='$FLASK_ADMIN_EMAIL', password='$FLASK_ADMIN_PASSWORD')
+    user_datastore.create_role(name='admin', description='administrator')
+    db.session.commit()
+    user = User.query.first()
+    role = Role.query.first()
+    user_datastore.add_role_to_user(user, role)
+    db.session.commit()
+    print(f'Admin is created!')"
+
 exec $cmd
