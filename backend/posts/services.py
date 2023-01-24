87f17b1dc828f2c9348aacfbf6943289da8ba3de
Peskov Sergey <devpeskov@proton.me>
2023-01-24 23:54:23 +0600

    Update logic in services: typehinting, search, exception

--- backend/posts/services.py
+++ backend/posts/services.py
@@ -1,6 +1,4 @@
-from typing import Any
-
-from sqlalchemy import func
+from flask_sqlalchemy.query import Query
 
 from app import db
 from models import Post, Tag
@@ -8,11 +6,11 @@ from models import Post, Tag
 from .forms import PostForm
 
 
-def get_filtered_posts(search_query: str) -> list[Any]:
+def get_filtered_posts(search_query: str) -> Query:
     if search_query:
-        return Post.query.filter(
-            func.lower(Post.title).contains(search_query.lower())
-            | func.lower(Post.body).contains(search_query.lower())
+        return db.session.query(Post).filter(
+            Post.title.ilike(f"%{search_query}%")
+            | Post.body.ilike(f"%{search_query}%")
         )
     else:
         return Post.query.order_by(Post.created.desc())
@@ -31,8 +29,8 @@ def add_post_to_db(post: Post) -> Post:
         db.session.add(post)
         db.session.commit()
         return post
-    except:
-        print("failed to send a create-query to the database")
+    except Exception as e:
+        print("<failed to send a create-query to the database:>", e)
 
 
 def update_post(form: PostForm) -> None:
